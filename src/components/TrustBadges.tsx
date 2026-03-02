ac923920c6615d230bf4400f15a856c2a507de72
James Brace <48393754+jbrace817@users.noreply.github.com>
2026-03-02 06:15:00 -0500

    style(TrustBadges): Remove unnecessary line break for cleaner layout

--- src/components/TrustBadges.tsx
+++ src/components/TrustBadges.tsx
@@ -142,7 +142,6 @@ export default function TrustBadges() {
                   />
                 </svg>
               </span>{" "}
-              <br className="hidden sm:block" />
               Us
             </h2>
 
